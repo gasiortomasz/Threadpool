c50a2c51863970927843da12a41e7cf0e40c2314
gasiortomasz <gasior.tt@gmail.com>
2020-12-29 10:14:40 +0100

    Get rid of atomics

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,53 +1,46 @@
 use std::ops::Deref;
-use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 use std::{collections::VecDeque, sync::MutexGuard};
 
 use thread::JoinHandle;
 
-type Task = Box<dyn FnOnce() + Send>;
+enum Message {
+    Task(Box<dyn FnOnce() + Send>),
+    Kill,
+}
 
 pub struct Threadpool {
     pool_size: u32,
     thread_handles: Vec<JoinHandle<()>>,
-    shared_state: Arc<(Mutex<VecDeque<Task>>, Condvar)>,
-    poison_pills: Arc<AtomicU32>,
+    shared_state: Arc<(Mutex<VecDeque<Message>>, Condvar)>,
 }
 
 impl Threadpool {
     pub fn new(pool_size: u32) -> Self {
-        let shared_state = Arc::new((Mutex::new(VecDeque::<Task>::new()), Condvar::new()));
-        let poison_pills = Arc::new(AtomicU32::new(0));
+        let shared_state = Arc::new((Mutex::new(VecDeque::<Message>::new()), Condvar::new()));
 
-        let predicate = |guard: &MutexGuard<VecDeque<Task>>, poison_counter: &Arc<AtomicU32>| {
-            guard.deref().is_empty() && poison_counter.load(Ordering::SeqCst) == 0
-        };
+        let predicate = |guard: &MutexGuard<VecDeque<Message>>| guard.deref().is_empty();
 
         let thread_handles = (0..pool_size)
             .map(|_| {
                 let cloned = shared_state.clone();
-                let atomic = poison_pills.clone();
 
                 thread::spawn(move || {
                     let (ref lock, ref cond) = *cloned;
                     loop {
                         let task = {
                             let mut task_queue = lock.lock().unwrap();
-                            while predicate(&task_queue, &atomic) {
+                            while predicate(&task_queue) {
                                 task_queue = cond.wait(task_queue).unwrap();
                             }
-
-                            match task_queue.deref().is_empty() {
-                                true => {
-                                    atomic.fetch_sub(1, Ordering::SeqCst);
-                                    break;
-                                }
-                                false => task_queue.pop_front().unwrap(),
-                            }
+                            task_queue.pop_front().unwrap()
                         };
                         cond.notify_all();
-                        task();
+                        match task {
+                            Message::Task(f) => f(),
+                            Message::Kill => break,
+                        }
                     }
                 })
             })
@@ -57,15 +50,18 @@ impl Threadpool {
             thread_handles,
             pool_size,
             shared_state,
-            poison_pills,
         }
     }
 
-    pub fn submit(&self, task: Task) {
+    pub fn submit<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
         let (ref lock, ref cond) = *self.shared_state;
         {
             let mut task_queue = lock.lock().unwrap();
-            task_queue.push_back(task);
+            let task = Box::new(f);
+            task_queue.push_back(Message::Task(task));
         }
         cond.notify_all();
     }
@@ -73,9 +69,11 @@ impl Threadpool {
 
 impl Drop for Threadpool {
     fn drop(&mut self) {
-        let (_, ref cond) = *self.shared_state;
-        self.poison_pills
-            .fetch_add(self.pool_size, Ordering::SeqCst);
+        let (ref lock, ref cond) = *self.shared_state;
+        {
+            let mut task_queue = lock.lock().unwrap();
+            (0..self.pool_size).for_each(|_| task_queue.push_back(Message::Kill));
+        }
         cond.notify_all();
 
         self.thread_handles
